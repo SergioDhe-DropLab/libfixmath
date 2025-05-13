56a5c692b42c23505fbadc1bb14d0a7804668001
SergioDhe-DropLab <134008599+SergioDhe-DropLab@users.noreply.github.com>
2025-05-13 12:07:58 -0400

    add definition guard for fix16_options

--- libfixmath/fix16_options.h
+++ libfixmath/fix16_options.h
@@ -2,11 +2,26 @@
 #define libfixmath_fix16_options_h__
 
 // // OPTION DEFINES FOR STM32 USE - Sergio
+#ifndef FIXMATH_NO_CTYPE
 #define FIXMATH_NO_CTYPE
+#endif
+
+#ifndef FIXMATH_FAST_SIN
 #define FIXMATH_FAST_SIN
+#endif
+
+#ifndef FIXMATH_NO_64BIT
 #define FIXMATH_NO_64BIT
+#endif
+
+#ifndef FIXMATH_NO_CACHE
 #define FIXMATH_NO_CACHE
+#endif
+
+#ifndef FIXMATH_NO_HARD_DIVISION
 #define FIXMATH_NO_HARD_DIVISION
+#endif
+
 // #define FIXMATH_NO_ROUNDING
 
 #endif /* libfixmath_fix16_options_h__ */
